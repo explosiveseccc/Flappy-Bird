189aeb483c33cf360c1acb178a5882b63eff83bf
explosiveseccc <nmartinez24@aoiths.org>
2022-04-13 08:54:24 -0400

    Initial commit

---
+++ Assets/Script/FlappyBird.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlappyBird : MonoBehaviour
+{
+    Rigidbody2D bird;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bird = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
